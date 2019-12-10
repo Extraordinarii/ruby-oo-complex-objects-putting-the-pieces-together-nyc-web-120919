3c30993b3f5ff7da88ae44c949886dbab8384004
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-10 18:37:27 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,14 @@
 # Make your shoe class here!
-class Shoe 
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+  def initialize(brand)
+    @brand = brand 
+    @color = color 
+    @size = size 
+    @material = material
+    @condition = condition
+  end 
+  def cobble
+    puts "Your shoe has been renewed!"
+    @condition()
 end 
