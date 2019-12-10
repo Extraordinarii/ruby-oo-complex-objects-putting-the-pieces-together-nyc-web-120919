626028d40f0a49d09223a882066c9311acd04967
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-12-10 18:33:44 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,15 @@
 class Book
-  attr_accessor :title, :author, :page_count
+  attr_accessor :title, :author, :page_count, :genre
   #attr_reader #is the getter 
   #attr_writer #is the setter 
+  def initialize(title)
+    @title = title 
+    @author = author 
+    @page_count = page_count
+    @genre = genre
+  end 
+  def turn_page
+    #turns the page 
+  end 
 end 
 
